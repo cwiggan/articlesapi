1cd661c214c10f9e51665529f0d2279f28561e5e
Carlita Wiggan <info@cwiggan.com>
2019-01-07 14:50:34 -0500

    articles api

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Article;
+use App\Http\Resources\Article as Articles;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $articles = Article::paginate(15);
+        return Articles::collection($articles);
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $article = new Article();
+        $article->title = $request->title;
+        $article->body = $request->body;
+
+        if ($article->save()) {
+            return new Articles($article);
+        }
+
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $article = Article::findOrFail($id);
+        return new Articles($article);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $article = Article::find($id);
+        $article->title = $request->title;
+        $article->body = $request->body;
+
+        if ($article->save()) {
+            return new Articles($article);
+        }
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $article = Article::findOrFail($id);
+        if ($article->delete()) {
+            return new Articles($article);
+        }
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+// articles api
+Route::prefix('article')->group(function () {
+    Route::get('/', 'ArticleController@index');
+    Route::get('{id}', 'ArticleController@show');
+    Route::post('/', 'ArticleController@store');
+    Route::put('{id}', 'ArticleController@update');
+    Route::delete('{id}', 'ArticleController@destroy');
+});
